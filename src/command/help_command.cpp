f0f3c397e0ea3cace9dca2d65386c5299e664333
dodoman8067 <dodoman8067@gmail.com>
2023-09-04 23:30:05 +0900

    Change std::exit to return

--- src/command/help_command.cpp
+++ src/command/help_command.cpp
@@ -13,7 +13,7 @@ void mcsm::HelpCommand::execute(const std::vector<std::string>& args){
     }else{
         if(!mcsm::CommandManager::hasCommand(args[0])){
             std::cout << "Unknown command \"" << args[0] << "\". Type 'mcsm help' for list of commands." << std::endl;
-            std::exit(0);
+            return;
         }
         std::unique_ptr<mcsm::Command> command = mcsm::CommandManager::getCommand(args[0]);
         std::cout << command->getName() << " - " << command->getDescription() <<  std::endl;
