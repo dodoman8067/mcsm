39dc6cb10712f88b4180c828b51ea2efbf250121
dodoman8067 <dodoman8067@gmail.com>
2023-11-02 21:25:20 +0900

    Capture proper exit code in mcsm::runCommand

--- src/util/cli/cli_utils.cpp
+++ src/util/cli/cli_utils.cpp
@@ -24,8 +24,20 @@ int mcsm::runCommandQuietly(const std::string& command){
 }
 
 int mcsm::runCommand(const std::string& command){
-    if(mcsm::getCurrentOS() == mcsm::OS::WINDOWS || mcsm::getCurrentOS() == mcsm::OS::LINUX){
+    if(mcsm::getCurrentOS() == mcsm::OS::WINDOWS){
         return std::system(command.c_str());
+    }else if(mcsm::getCurrentOS() == mcsm::OS::LINUX){
+        #ifdef __linux__
+            int code = std::system(command.c_str());
+            if(WIFEXITED(code)){
+                int finalCode = WEXITSTATUS(code);
+                return finalCode;
+            }else{
+                std::cerr << "[mcsm] Command " << command << " failed.\n";
+                std::cerr << "If you believe this is a bug, please report it to GitHub (https://github.com/dodoman8067/mcsm).";
+            }
+        #endif
+        return 1;
     }else{
         std::cerr << "[mcsm] This platform is not supported. Please use Windows or Linux.\n";
         std::exit(1);
