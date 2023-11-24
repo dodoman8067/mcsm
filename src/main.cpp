55f124c737e1383ffce0cc152d34f55f6b19c59c
dodoman8067 <dodoman8067@gmail.com>
2023-11-24 21:44:57 +0900

    Add license comment

--- src/main.cpp
+++ src/main.cpp
@@ -19,10 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include <iostream>
-#include <vector>
-#include <memory>
-#include <string>
+
 #include <mcsm/command/command_manager.h>
 #include <mcsm/command/base/command.h>
 #include <mcsm/command/util/version_command.h>
