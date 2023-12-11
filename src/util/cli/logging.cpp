b15bf9129f62a0e90920d635322984a6ea6a51d5
dodoman8067 <dodoman8067@gmail.com>
2023-12-12 08:27:23 +0900

    Comment some code

--- src/util/cli/logging.cpp
+++ src/util/cli/logging.cpp
@@ -22,18 +22,34 @@ SOFTWARE.
 
 #include <mcsm/util/cli/logging.h>
 
+/**
+ * Prints normal information message to the console.
+ * @param message the string to print
+*/
 void mcsm::info(const std::string& message){
     std::cout << "[mcsm/INFO] " << message << "\n";
 }
 
+/**
+ * Prints successful (green) message to the console.
+ * @param message the string to print
+*/
 void mcsm::success(const std::string& message){
     std::cout << termcolor::bright_green << "[mcsm/INFO] " << message << termcolor::reset << "\n";
 }
 
+/**
+ * Prints warning (yellow) message to the console.
+ * @param message the string to print
+*/
 void mcsm::warning(const std::string& message){
     std::cerr << termcolor::bright_yellow << "[mcsm/WARN] " << message << termcolor::reset << "\n";
 }
 
+/**
+ * Prints error (red) message to the console.
+ * @param message the string to print
+*/
 void mcsm::error(const std::string& message){
     std::cerr << termcolor::bright_red << "[mcsm/ERROR] " << message << termcolor::reset << "\n";
 }
