7d2933bcd1b714142bd65520a4690e311c662575
dodoman8067 <dodoman8067@gmail.com>
2024-02-02 23:23:11 +0900

    Gonna do it someday

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]){
     //libcurl-linux : cmake -B ./build -DBUILD_SHARED_LIBS=OFF -DOPENSSL_USE_STATIC_LIBS=ON -DZLIB_USE_STATIC_LIBS=ON
     //libcurl : cmake command : cmake -B ./build -G "MinGW Makefiles" -DBUILD_SHARED_LIBS=OFF -DZLIB_USE_STATIC_LIBS=ON -DCURL_USE_SCHANNEL=ON
 
-    //TODO : Default op in server config, spiget.org
+    //TODO : Default op in server config, spiget.org and add server on other directory download support
 
     mcsm::init init;
     init.initMCSM(version);
