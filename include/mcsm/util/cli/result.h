86c1b958e5fc83de285fed72a07024ad2e3fae51
dodoman8067 <dodoman8067@gmail.com>
2024-02-25 00:23:27 +0900

    Add some other error messages

--- include/mcsm/util/cli/result.h
+++ include/mcsm/util/cli/result.h
@@ -141,6 +141,24 @@ namespace mcsm {
             };
         }
 
+        inline std::vector<std::string> serverDefaultProfileNotFound(const std::string& name){
+            return {
+                "No default launch profile specified in file " + name,
+                "Manually editing the launch profile might have caused this issue.",
+                "If you know what you're doing, I believe you that you know how to handle this issue.",
+                "If you believe that this is a software issue, please report this to GitHub (https://github.com/dodoman8067/mcsm)."
+            };
+        }
+
+        inline std::vector<std::string> serverDefaultProfileNameNotFound(const std::string& name){
+            return {
+                "No default launch profile name specified in file " + name,
+                "Manually editing the launch profile might have caused this issue.",
+                "If you know what you're doing, I believe you that you know how to handle this issue.",
+                "If you believe that this is a software issue, please report this to GitHub (https://github.com/dodoman8067/mcsm)."
+            };
+        }
+
         inline std::vector<std::string> serverUnsupportedVersion(const std::string& build){
             return {
                 "Unsupported version : " + build,
