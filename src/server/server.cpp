4020ace2d23d3b810b76ae37893e3c5106c11f81
dodoman8067 <dodoman8067@gmail.com>
2024-02-27 23:17:33 +0900

    Use mcsm::Result in error handling

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -44,15 +44,24 @@ std::string mcsm::Server::getJarFile() const {
 
 std::string mcsm::Server::getJarFile(const std::string& checkDir) const {
     mcsm::Option opt(checkDir, "server");
-    if(opt.exists() && opt.getValue("server_jar") != nullptr){
-        if(!opt.getValue("server_jar").is_string()){
-            mcsm::error("Value \"server_jar\" has to be a string, but it's not.");
-            mcsm::error("Manually editing the launch profile might have caused this issue.");
-            mcsm::error("If you know what you're doing, I believe you that you know how to handle this issue.");
-            mcsm::error("If you believe that this is a software issue, please report it to GitHub (https://github.com/dodoman8067/mcsm).");
-            std::exit(1);
+    bool exists = opt.exists();
+    if(mcsm::getLastResult().first != mcsm::ResultType::MCSM_OK && mcsm::getLastResult().first != mcsm::ResultType::MCSM_SUCCESS) return "";
+
+    if(exists){
+        nlohmann::json value = opt.getValue("server_jar");
+        if(mcsm::getLastResult().first != mcsm::ResultType::MCSM_OK && mcsm::getLastResult().first != mcsm::ResultType::MCSM_SUCCESS) return "";
+
+        if(value != nullptr){
+            mcsm::Result res({mcsm::ResultType::MCSM_FAIL, mcsm::message_utils::jsonNotFound("\"server_jar\"", opt.getName())});
+            return "";
         }
-        return opt.getValue("server_jar");
+
+        if(!value.is_string()){
+            mcsm::Result res({mcsm::ResultType::MCSM_FAIL, mcsm::message_utils::jsonWrongType("\"server_jar\"", "string")});
+            return "";
+        }
+        mcsm::Result res({mcsm::ResultType::MCSM_SUCCESS, {"Success"}});
+        return value;
     }
     return getTypeAsString() + ".jar";
 }
