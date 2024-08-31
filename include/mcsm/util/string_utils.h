26c74686af53b432072d32f0baefb858edcc90ab
dodoman8067 <dodoman8067@gmail.com>
2024-08-31 12:05:48 +0900

    Add is_number

--- include/mcsm/util/string_utils.h
+++ include/mcsm/util/string_utils.h
@@ -59,6 +59,8 @@ namespace mcsm {
     void replaceAll(std::string& str, const std::string& value, const std::string& replacement);
 
     std::string safeString(const std::string& str);
+
+    bool is_number(const std::string& s);
 }
 
 #endif
--- src/util/string_utils.cpp
+++ src/util/string_utils.cpp
@@ -65,3 +65,9 @@ std::string mcsm::safeString(const std::string& str){
     mcsm::replaceAll(value, "\'", "_");
     return value;
 }
+
+bool mcsm::is_number(const std::string& s){
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
