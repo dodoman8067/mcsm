624dbc604e180fb898b3f0491232b0f97808d30f
dodoman8067 <dodoman8067@gmail.com>
2024-10-03 23:06:04 +0900

    Update version and add 0.4 plans

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,20 @@ SOFTWARE.
 #include <mcsm/init.h>
 //#include <mcsm/data/options/multi_server_option.h>
 
-const std::string version = "0.3";
+const std::string version = "0.4";
 
 int main(int argc, char *argv[]){
     /**
      * TODO
-     * Check if the name of each linked server in mcsm::MultiServerOption does not match the name of any other server
-     * Implement mcsm update command.
-     * Add multicurl task and make an internal method that returns CURL handle on Server class (obtainJarFile)
+     * Add global settings
+     * Example
+     * skip_version_check_while_configuring: Skip hasVersion check while configuring. Can be used to reduce configuring time (more than 10 secs)
+     * enable_beta_features: Enable beta fetures by implementing hasBetaEnabled() 
+     * 
+     * Add an option to toggle copying the server jarfile when filepath is given on custom server configurations
+     * 
+     * Add a function like std::pair<std::string, std::string> getRequirements() to the server classes. 
+     * This will be used by the server generator command to define unique parameters required by each server type.
      */
 
     //libssh2 : cmake -B ./build -DBUILD_SHARED_LIBS=OFF -DOPENSSL_USE_STATIC_LIBS=ON -DZLIB_USE_STATIC_LIBS=ON -DENABLE_ZLIB_COMPRESSION=ON -DCRYPTO_BACKEND=OpenSSL
