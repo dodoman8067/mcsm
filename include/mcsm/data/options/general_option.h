6bb263af175b788154de3f7f147d8b4808920990
dodoman8067 <dodoman8067@gmail.com>
2024-10-07 21:19:03 +0900

    Implement add/remove property

--- include/mcsm/data/options/general_option.h
+++ include/mcsm/data/options/general_option.h
@@ -2,6 +2,7 @@
 #define __MCSM_GENERAL_OPTION_H__
 
 #include <mcsm/data/global_option.h>
+#include <algorithm>
 #include <mcsm/data/options/general_property.h>
 
 // how it is gonna be implemented:
@@ -10,25 +11,31 @@
 namespace mcsm {
     class GeneralOption {
     private:
-        GeneralOption();
-        ~GeneralOption();
+        GeneralOption() = default;
 
         std::unique_ptr<mcsm::GlobalOption> optionHandle;
-        std::vector<mcsm::GeneralProperty> properties;
+        std::vector<mcsm::GeneralProperty*> properties;
 
         mcsm::Result save();
+        mcsm::Result load();
     public:
+        GeneralOption(const mcsm::GeneralOption&) = delete;
+        GeneralOption& operator=(const mcsm::GeneralOption&) = delete;
+        ~GeneralOption() = default;
+
         // initializes optionHandle
         static mcsm::Result initialize();
         // returns instance of GeneralOption
-        static mcsm::GeneralOption* getGeneralOption();
+        static mcsm::GeneralOption& getGeneralOption();
 
-        std::vector<mcsm::GeneralProperty>& getProperties() const;
+        std::vector<mcsm::GeneralProperty*>& getProperties() const;
 
         mcsm::Result addProperty(mcsm::GeneralProperty& property);
         mcsm::Result removeProperty(const std::string& propertyName);
 
         mcsm::Result setProperty(const std::string& propertyName, nlohmann::json newValue);
+
+        mcsm::GlobalOption* getOption();
     };
 }
 #endif // __MCSM_GENERAL_OPTION_H__
--- src/data/options/general_option.cpp
+++ src/data/options/general_option.cpp
@@ -1,1 +1,63 @@
 #include <mcsm/data/options/general_option.h>
+
+static auto& instance = mcsm::GeneralOption::getGeneralOption();
+static bool init = false;
+
+// loads poperties vector from json
+mcsm::Result mcsm::GeneralOption::load(){
+    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+}
+
+// saves current properties vector value to json
+mcsm::Result mcsm::GeneralOption::save(){
+    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+}
+
+mcsm::Result mcsm::GeneralOption::initialize(){
+    if(init){
+        return {mcsm::ResultType::MCSM_FAIL, {
+            "GeneralOption::initialize() called more than once.",
+            "Likely an internal issue. Open an issue at Github."
+        }};
+    }
+    init = true;
+    
+    return instance.load();
+}
+
+mcsm::Result mcsm::GeneralOption::addProperty(mcsm::GeneralProperty& property){
+    instance.properties.push_back(&property);
+    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+}
+
+mcsm::Result mcsm::GeneralOption::removeProperty(const std::string& propertyName){
+    properties.erase(std::remove_if(properties.begin(), properties.end(),
+        [&](mcsm::GeneralProperty* property){
+            return property->getName() == propertyName;
+        }), properties.end());
+
+    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+}
+
+mcsm::Result mcsm::GeneralOption::setProperty(const std::string& propertyName, nlohmann::json newValue){
+    for(auto& v : instance.properties){
+        if(v->getName() == propertyName){
+            v->setCurrentValue(newValue);
+            break;
+        }
+    }
+    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+}
+
+mcsm::GlobalOption* mcsm::GeneralOption::getOption(){
+    return &(*instance.optionHandle);
+}
+
+std::vector<mcsm::GeneralProperty*>& mcsm::GeneralOption::getProperties() const {
+    return instance.properties;
+}
+
+mcsm::GeneralOption& mcsm::GeneralOption::getGeneralOption(){
+    static mcsm::GeneralOption o;
+    return o;
+}
