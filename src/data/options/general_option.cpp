cb6525dc38c999c38d49b9ffa813bd002f366c12
dodoman8067 <dodoman8067@gmail.com>
2024-10-09 10:27:38 +0900

    Recover registered but non existing properties in general options

--- src/data/options/general_option.cpp
+++ src/data/options/general_option.cpp
@@ -5,29 +5,28 @@ static bool init = false;
 
 // loads poperties vector from json
 mcsm::Result mcsm::GeneralOption::load(){
-    bool exists = instance.optionHandle->exists();
-    if(mcsm::getLastResult().first != mcsm::ResultType::MCSM_OK && mcsm::getLastResult().first != mcsm::ResultType::MCSM_SUCCESS){
-        std::pair<mcsm::ResultType, std::vector<std::string>> resp = mcsm::getLastResult();
-        mcsm::Result res(resp.first, resp.second);
-        return res;
-    }
-    if(!exists){
-        nlohmann::json json;
-
-        for(auto* v : mcsm::ServerRegistry::getServerRegistry().getRegisteredProperties()){
-            json[v->getName()] = v->getDefaultValue();
-        }
-
-        mcsm::Result saveRes = instance.optionHandle->save(json);
-
-        if(!saveRes.isSuccess()) return saveRes;
-    }
-
     nlohmann::json json = instance.optionHandle->load();
     if(mcsm::getLastResult().first != mcsm::ResultType::MCSM_OK && mcsm::getLastResult().first != mcsm::ResultType::MCSM_SUCCESS){
         std::pair<mcsm::ResultType, std::vector<std::string>> resp = mcsm::getLastResult();
-        mcsm::Result res(resp.first, resp.second);
-        return res;
+        //if(resp.second[0].find("parse json") == std::string::npos){
+            mcsm::Result res(resp.first, resp.second);
+            return res;
+        //}
+    }
+
+    bool recovered = false;
+
+    for(auto* v : mcsm::ServerRegistry::getServerRegistry().getRegisteredProperties()){
+        if(json[v->getName()] == nullptr){ 
+            json[v->getName()] = v->getDefaultValue();
+            recovered = true; 
+        }
+    }
+
+    if(recovered){
+        mcsm::Result saveRes = instance.optionHandle->save(json);
+
+        if(!saveRes.isSuccess()) return saveRes;
     }
 
     for(auto it = json.begin(); it != json.end(); ++it){
@@ -36,7 +35,7 @@ mcsm::Result mcsm::GeneralOption::load(){
             property->setCurrentValue(it.value());
             instance.properties.push_back(property);
         }else{
-            return {mcsm::ResultType::MCSM_FAIL, {"Property " + it.key() + " is null"}};
+            return {mcsm::ResultType::MCSM_FAIL, {"Non-registered general property " + it.key() + " detected."}};
         }
     }
 
