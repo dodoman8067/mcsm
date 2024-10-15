791456017fe3c982c71e01a49acd706b23e29578
dodoman8067 <dodoman8067@gmail.com>
2024-10-15 20:13:03 +0900

    Add a property

---
+++ include/mcsm/data/options/general/advanced_json_errors_property.h
@@ -0,0 +1,18 @@
+#ifndef __MCSM_ADVANCED_JSON_ERRORS_PROPERTY_H__
+#define __MCSM_ADVANCED_JSON_ERRORS_PROPERTY_H__
+
+#include <mcsm/data/options/general_option.h>
+
+namespace mcsm {
+    class AdvancedJsonErrorsProperty : public mcsm::GeneralProperty {
+    public:
+        AdvancedJsonErrorsProperty(const std::string& name): GeneralProperty(name){}
+        ~AdvancedJsonErrorsProperty() = default;
+
+        nlohmann::json getDefaultValue() override {
+            return false;
+        }
+    };
+}
+
+#endif // __MCSM_ADVANCED_JSON_ERRORS_PROPERTY_H__
