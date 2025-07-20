e2da92dc829ba7e9011300f442414fba0d8a2d26
dodoman8067 <dodoman8067@gmail.com>
2025-07-20 13:23:01 +0900

    Migrate general option handler to mcsm::Error

--- src/data/options/general_option.cpp
+++ src/data/options/general_option.cpp
@@ -6,17 +6,10 @@ static bool init = false;
 // loads poperties vector from json
 mcsm::VoidResult mcsm::GeneralOption::load(){
     instance.properties.reserve(3);
-    mcsm::Result jLoadRes = instance.optionHandle->load();
-    if(!jLoadRes.isSuccess()) return jLoadRes;
+    mcsm::VoidResult jLoadRes = instance.optionHandle->load();
+    if(!jLoadRes) return jLoadRes;
 
     nlohmann::json json = instance.optionHandle->getData();
-    if(mcsm::getLastResult().first != mcsm::ResultType::MCSM_OK && mcsm::getLastResult().first != mcsm::ResultType::MCSM_SUCCESS){
-        std::pair<mcsm::ResultType, std::vector<std::string>> resp = mcsm::getLastResult();
-        //if(resp.second[0].find("parse json") == std::string::npos){
-            mcsm::Result res(resp.first, resp.second);
-            return res;
-        //}
-    }
 
     bool recovered = false;
 
@@ -28,50 +21,49 @@ mcsm::VoidResult mcsm::GeneralOption::load(){
     }
 
     if(recovered){
-        mcsm::Result saveRes = instance.optionHandle->save(json);
+        mcsm::VoidResult saveRes = instance.optionHandle->save(json);
 
-        if(!saveRes.isSuccess()) return saveRes;
+        if(!saveRes) return saveRes;
     }
 
     for(auto it = json.begin(); it != json.end(); ++it){
         auto property = mcsm::ServerRegistry::getServerRegistry().getGeneralProperty(it.key());
-        if(property){
-            property->setCurrentValue(it.value());
-            instance.properties.push_back(property);
-        }else{
-            return {mcsm::ResultType::MCSM_FAIL, {"Non-registered general property " + it.key() + " detected."}};
+        if(!property){
+            return tl::unexpected(property.error());
         }
+        property.value()->setCurrentValue(it.value());
+        instance.properties.push_back(property.value());
     }
 
     auto* property = getProperty("advanced_json_parse_fail_errors");
     if(property == nullptr){
-        mcsm::Result res({mcsm::ResultType::MCSM_FAIL, mcsm::message_utils::jsonNotFoundPlusFix("advanced_json_parse_fail_errors", "general option", "\"advanced_json_parse_fail_errors\": false")});
-        return res;
+        mcsm::Error err = mcsm::makeError(mcsm::ErrorStatus::ERROR, mcsm::errors::JSON_NOT_FOUND_PLUS_FIX, {"advanced_json_parse_fail_errors", "general option", "Value example: \"advanced_json_parse_fail_errors\": false"});
+        return tl::unexpected(err);
     }
 
     const nlohmann::json& propertyValue = property->getCurrentValue();
     if(!propertyValue.is_boolean()){
-        mcsm::Result res({mcsm::ResultType::MCSM_FAIL, mcsm::message_utils::jsonWrongTypePlusFix("advanced_json_parse_fail_errors", "general option", "boolean", "false or true")});
-        return res;
+        mcsm::Error err = mcsm::makeError(mcsm::ErrorStatus::ERROR, mcsm::errors::JSON_WRONG_TYPE_PLUS_FIX, {"advanced_json_parse_fail_errors", "general option", "boolean", "Value example: \"advanced_json_parse_fail_errors\": false"});
+        return tl::unexpected(err);
     }
 
     this->advp = propertyValue;
 
     auto* property1 = getProperty("screen_binary_path");
     if(property1 == nullptr){
-        mcsm::Result res({mcsm::ResultType::MCSM_FAIL, mcsm::message_utils::jsonNotFoundPlusFix("screen_binary_path", "general option", "\"screen_binary_path\": \"/usr/bin/screen\"")});
-        return res;
+        mcsm::Error err = mcsm::makeError(mcsm::ErrorStatus::ERROR, mcsm::errors::JSON_NOT_FOUND_PLUS_FIX, {"screen_binary_path", "general option", "Value example: \"/usr/bin/screen\""});
+        return tl::unexpected(err);
     }
 
     const nlohmann::json& propertyValue1 = property1->getCurrentValue();
     if(!propertyValue1.is_string()){
-        mcsm::Result res({mcsm::ResultType::MCSM_FAIL, mcsm::message_utils::jsonWrongTypePlusFix("screen_binary_path", "general option", "string", "\"/usr/bin/screen\"")});
-        return res;
+        mcsm::Error err = mcsm::makeError(mcsm::ErrorStatus::ERROR, mcsm::errors::JSON_WRONG_TYPE_PLUS_FIX, {"screen_binary_path", "general option", "string", "Value example: \"/usr/bin/screen\""});
+        return tl::unexpected(err);
     }
 
     this->sbp = propertyValue1;
 
-    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+    return {};
 }
 
 // saves current properties vector value to json
@@ -82,26 +74,19 @@ mcsm::VoidResult mcsm::GeneralOption::save(){
         json[v->getName()] = v->getCurrentValue();
     }
 
-    mcsm::Result saveRes = instance.optionHandle->save(json);
+    mcsm::VoidResult saveRes = instance.optionHandle->save(json);
 
     return saveRes;
 }
 
 mcsm::VoidResult mcsm::GeneralOption::initialize(){
     if(init){
-        return {mcsm::ResultType::MCSM_FAIL, {
-            "GeneralOption::initialize() called more than once.",
-            "Likely an internal issue. Open an issue at Github."
-        }};
+        mcsm::Error err = mcsm::makeError(mcsm::ErrorStatus::ERROR, mcsm::errors::INTERNAL_FUNC_EXECUTION_FAILED, {"GeneralOption::initialize()", "Called more than once"});
+        return tl::unexpected(err);
     }
     init = true;
     
     instance.optionHandle = std::make_unique<mcsm::Option>(mcsm::asGlobalConfigPath("/"), "general_options");
-    if(mcsm::getLastResult().first != mcsm::ResultType::MCSM_OK && mcsm::getLastResult().first != mcsm::ResultType::MCSM_SUCCESS){
-        std::pair<mcsm::ResultType, std::vector<std::string>> resp = mcsm::getLastResult();
-        mcsm::Result res(resp.first, resp.second);
-        return res;
-    }
 
     return instance.load();
 }
@@ -122,7 +107,7 @@ mcsm::VoidResult mcsm::GeneralOption::setProperty(const std::string& propertyNam
             break;
         }
     }
-    return {mcsm::ResultType::MCSM_SUCCESS, {"Success"}};
+    return {};
 }
 
 mcsm::Option* mcsm::GeneralOption::getOption(){
