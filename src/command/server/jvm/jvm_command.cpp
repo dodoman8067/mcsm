314fa47064e841c964d83febd209e4c544182e3a
dodoman8067 <dodoman8067@gmail.com>
2025-09-02 23:25:11 +0900

    Add base jvm command code

---
+++ src/command/server/jvm/jvm_command.cpp
@@ -0,0 +1,41 @@
+#include <mcsm/command/server/jvm/jvm_command.h>
+
+mcsm::JvmCommand::JvmCommand(const std::string& name, const std::string& description) : mcsm::Command(name, description){}
+
+mcsm::JvmCommand::~JvmCommand(){
+
+}
+
+void mcsm::JvmCommand::execute(const std::vector<std::string>& args){
+    if(args.empty()){
+        mcsm::warning("Invalid command.");
+        mcsm::warning("Type mcsm jvm help for more information.");
+        std::exit(1);
+    }
+    std::string subc = args[0];
+    std::vector<std::string> subArgs;
+    for(size_t i = 1; i < args.size(); i++){
+        subArgs.push_back(args[i]);
+    }
+
+    // will handle string valid checks on command classes.
+    // only handle them here if you must
+    if(subc == "create"){
+    }
+    if(subc == "edit"){
+    }
+    if(subc == "detect-install"){
+    }
+    if(subc == "search"){
+    }
+    if(subc == "import"){
+    }
+    if(subc == "test"){
+    }
+    if(subc == "help"){
+
+    }
+    mcsm::warning("Invalid command.");
+    mcsm::warning("Type mcsm jvm help for more information.");
+    std::exit(1);
+}
