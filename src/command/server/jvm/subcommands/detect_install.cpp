3b014f03433e76c246e2c5e8e9d84ec7845030bf
dodoman8067 <dodoman8067@gmail.com>
2026-01-17 21:00:53 +0900

    Display no java detected message

--- src/command/server/jvm/subcommands/detect_install.cpp
+++ src/command/server/jvm/subcommands/detect_install.cpp
@@ -4,12 +4,22 @@
 void mcsm::JvmDetectInstallSubCommand::execute(const std::vector<std::string>& /* args */){
     mcsm::info("This is a simple utility that tries to detect installed Java virtual machine binaries.");
     mcsm::info("Note that it may not catch manual portable installs and such. You may define MCSM_JAVA_PATHS environment variable with semicolon(;) separators to let MCSM detect custom locations.");
-    mcsm::success("Detected Java virtual machine binaries: ");
-    for(const std::string& java : mcsm::findJavaPaths()){
+
+    auto javas = mcsm::findJavaPaths();
+    std::set<std::string> validJavas = {};
+    for(const std::string& java : javas){
         auto isValid = mcsm::unwrapOrExit(mcsm::isValidJava(java));
         if(isValid){
-            std::cout << " * " << java << "\n";
+            validJavas.insert({java});
         }
     }
+
+    if(!validJavas.empty()){ mcsm::success("Detected Java virtual machine binaries: "); }
+    else{ mcsm::warning("No java seems to be detected for now!"); }
+
+    for(const std::string& java2 : validJavas){
+        std::cout << " * " << java2 << "\n"; 
+    }
+
     std::exit(0);
 }
